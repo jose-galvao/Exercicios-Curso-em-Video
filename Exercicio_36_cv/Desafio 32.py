a63a69b00b59b629e93fa867fe491a4074c50610
José Silvestre da Silva Galvão <josesilvestregalvao2012@gmail.com>
2021-09-23 15:46:37 -0300

    desafio concluido

---
+++ Exercicio_36_cv/Desafio 32.py
@@ -0,0 +1,12 @@
+valorcasa = float(input('Digite o valor da casa: '))
+salario = float(input('Digite o seu salario: '))
+anos = int(input('Em quantos anos você vai pagar a casa?'))
+
+prestacao = anos * 12
+mensal = valorcasa / prestacao
+porcentagem = (mensal * 100) / salario
+
+if porcentagem <= 30:
+    print('Você pode comprar a casa, sua parcela mensal será de {} reais.'.format(mensal))
+else:
+    print('você não pode comprar essa casa, por exceder o limite de 30% do seu salario mensal!')
