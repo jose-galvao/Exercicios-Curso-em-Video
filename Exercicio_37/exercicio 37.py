f3d994e1ac2795adedf5cac6e1a5aacebce3a41e
José Silvestre da Silva Galvão <josesilvestregalvao2012@gmail.com>
2021-09-23 16:29:01 -0300

    inicio do exercicio

---
+++ Exercicio_37/exercicio 37.py
@@ -0,0 +1,11 @@
+numero = int(input('Digite um número: '))
+basedec = int(input('Qual base de conversão você deseja? \n 1 - Biário \n 2 - Octal \n 3 - Hexadecimal \n 4'))
+
+if basedec == 1:
+    print("sdasfsdf")
+elif basedec == 2:
+    print('oi')
+elif basedec == 3:
+    print('jsfg')
+else:
+    print('Número inválido.')
