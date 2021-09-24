6db98acd96625fdff324ec7b1c2d53a0cb68ba73
José Silvestre da Silva Galvão <josesilvestregalvao2012@gmail.com>
2021-09-24 14:37:13 -0300

    Exercicio 38 okay

---
+++ Exercicio_36_cv/Exercicio_38/exercicio 38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 < n2:
+    print('O segundo numero é maior')
+else:
+    print('Não existe vlor maior, os dois números são iguis')
