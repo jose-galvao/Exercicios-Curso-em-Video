8b3f9bd08bf3fdeb80d9805fbcaa749130dee65c
José Silvestre da Silva Galvão <josesilvestregalvao2012@gmail.com>
2021-09-24 14:30:10 -0300

    concluído

--- Exercicio_37/exercicio 37.py
+++ Exercicio_37/exercicio 37.py
@@ -1,11 +1,11 @@
 numero = int(input('Digite um número: '))
-basedec = int(input('Qual base de conversão você deseja? \n 1 - Biário \n 2 - Octal \n 3 - Hexadecimal \n 4'))
+basedec = int(input('Qual base de conversão você deseja? \n 1 - Biário \n 2 - Octal \n 3 - Hexadecimal \n '))
 
 if basedec == 1:
-    print("sdasfsdf")
+    print("A coversão de {} para binario é : {}".format(numero, bin(numero)[2:]))
 elif basedec == 2:
-    print('oi')
+    print('A conversão de {} para octal é : {}'.format(numero, oct(numero)[2:]))
 elif basedec == 3:
-    print('jsfg')
+    print('A conversão de {} para hexadecimal é: {}'.format(numero, hex(numero)[2:]))
 else:
     print('Número inválido.')
